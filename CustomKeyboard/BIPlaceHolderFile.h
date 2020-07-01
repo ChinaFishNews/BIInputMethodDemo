c3a6683c3262453090e49a9e02c42bd596cb89d9
yuxinwen <yuxinwen@baidu.com>
2020-07-01 19:52:38 +0800

    test

---
+++ CustomKeyboard/BIPlaceHolderFile.h
@@ -0,0 +1,17 @@
+//
+//  BIPlaceHolderFile.h
+//  CustomKeyboard
+//
+//  Created by xinwen on 2020/7/1.
+//  Copyright © 2020 baidu. All rights reserved.
+//  这个类纯占位使用，没有任何功能性作用，CustomKeyboard下如果没有代码文件，会导致编译不过
+
+#import <Foundation/Foundation.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface BIPlaceHolderFile : NSObject
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ CustomKeyboard/BIPlaceHolderFile.m
@@ -0,0 +1,13 @@
+//
+//  BIPlaceHolderFile.m
+//  CustomKeyboard
+//
+//  Created by xinwen on 2020/7/1.
+//  Copyright © 2020 baidu. All rights reserved.
+//
+
+#import "BIPlaceHolderFile.h"
+
+@implementation BIPlaceHolderFile
+
+@end
