aa9ba4aa5071374089447f7c02928539bab24178
yuxinwen <yuxinwen@baidu.com>
2020-07-01 14:08:02 +0800

    Initial Commit

---
+++ BIInputMethodDemo/SceneDelegate.h
@@ -0,0 +1,16 @@
+//
+//  SceneDelegate.h
+//  BIInputMethodDemo
+//
+//  Created by xinwen on 2020/7/1.
+//  Copyright © 2020 baidu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SceneDelegate : UIResponder <UIWindowSceneDelegate>
+
+@property (strong, nonatomic) UIWindow * window;
+
+@end
+
---
+++ BIInputMethodDemo/ViewController.m
@@ -0,0 +1,23 @@
+//
+//  ViewController.m
+//  BIInputMethodDemo
+//
+//  Created by xinwen on 2020/7/1.
+//  Copyright © 2020 baidu. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+
+@end
